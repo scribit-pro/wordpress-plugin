58d39f6d532146df8970508dfb1cf8766634c67e
Nico Stapelbroek <git@nstapelbroek.com>
2023-02-28 13:23:58 +0100

    initial setup new wordpress plugin

---
+++ plugin.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Name:       Scribit.pro
+ * Plugin URI:        https://github.com/scribit-pro/wordpress-plugin
+ * Description:       Online video from your WordPress site accessible to all.
+ * Version:           0.1
+ * Requires at least: 5.9
+ * Requires PHP:      8.0
+ * Author:            scribit.pro
+ * Author URI:        https://scribit.pro
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       scribit
+ *
+ * @package scribit
+ */
+
+namespace Scribit\WordPress;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
