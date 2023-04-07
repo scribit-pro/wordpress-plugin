8f92e165ed3059b77b805a2e19914e2674900777
Nico Stapelbroek <git@nstapelbroek.com>
2023-04-07 17:36:42 +0200

    chore: setup testing framework

---
+++ tests/TestScribitID.php
@@ -0,0 +1,12 @@
+<?php
+
+class SampleTest extends WP_UnitTestCase {
+
+    /**
+     * A single example test.
+     */
+    function test_sample() {
+        // Replace this with some actual testing code.
+        $this->assertTrue( true );
+    }
+}
